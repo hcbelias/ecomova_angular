d5cd56fa9369ce1f36c5546f47da94893c27a870
Henrique Elias <hcbe2004@gmail.com>
2014-02-22 01:12:43 -0300

    Cria;'ao projeto angular JS

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ecomovaJsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/my_places.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ecomovaJsApp')
+  .controller('MyPlacesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'My places',
+      'AngularJS',
+      'Karma'
+    ];
+  });
